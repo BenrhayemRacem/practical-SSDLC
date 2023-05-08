0aa44ba1576160d1c9713ccb64a5a8df308bff16
benrhayemracem <rassouma74@gmail.com>
2023-05-08 18:50:39 +0100

    monitor event loop + prevent HTTP parameter pollution

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
-
+import { ServiceUnavailableException, ValidationPipe } from '@nestjs/common';
+import toobusy from 'toobusy-js';
+import * as hpp from 'hpp';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.use(function (req, res, next) {
+    if (toobusy()) {
+      throw new ServiceUnavailableException('Service unavailable right now');
+    } else {
+      next();
+    }
+  });
+  app.use(hpp());
   app.enableCors();
-  app.useGlobalPipes(new ValidationPipe({
-    transform : true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
